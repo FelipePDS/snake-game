491b9ba53534b32a87b0f0a196bba7108b80755c
FelipePDS <felipepdasilva66@gmail.com>
2021-03-07 20:17:54 -0300

    declate initial values

--- game.js
+++ game.js
@@ -1,13 +1,13 @@
-const box = document.querySelector('#box')
-const context = box.getContext('2d')
+// DECLARE VALUES
+const game = {
+    global: {
+        box: document.querySelector('#box'),
+        context: this.box.getContext('2d')
+    }
+}
 
-context.fillStyle = `${themes.modes[localStorage.Theme].colorSnake}`
-context.fillRect(50, 10, 5, 5)
-context.fillRect(50, 16, 5, 5)
-context.fillRect(50, 22, 5, 5)
-context.fillRect(50, 28, 5, 5)
-context.fillRect(50, 34, 5, 5)
+game.global.box.width = 1200
+game.global.box.height = 1000
 
-context.fillStyle = `${themes.modes[localStorage.Theme].colorFood}`
-context.fillRect(230, 100, 5, 5)
-context.fillRect(51.5, 8, 2, 3)
+// game.global.context.fillStyle = 'red'
+// game.global.context.fillRect(10, 10, 30, 30)
