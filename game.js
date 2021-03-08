bca5d1692d1022ca7c23f2672480f56fab36b43b
FelipePDS <felipepdasilva66@gmail.com>
2021-03-07 23:17:11 -0300

    declare values

--- game.js
+++ game.js
@@ -1,13 +1,33 @@
-// DECLARE VALUES
 const game = {
     global: {
         box: document.querySelector('#box'),
         context: this.box.getContext('2d')
+    },
+
+    status: {
+        buttonStatusGame: document.querySelector('#statusGame'),
+        pause: true
+    },
+
+    entities: {
+        player: {
+            position: () => {
+                return game.global.context
+            },
+
+            ponctuation: {
+                content: document.querySelector('.ponctuation'),
+                value: 1
+            }
+        },
+
+        food: () => {
+            return game.global.context
+        }
     }
 }
 
+// ======================== // ======================== //
+
 game.global.box.width = 1200
 game.global.box.height = 1000
-
-// game.global.context.fillStyle = 'red'
-// game.global.context.fillRect(10, 10, 30, 30)
