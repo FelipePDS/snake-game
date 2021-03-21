5dbbd5f291fa5154f876a8502003facf818bbf24
FelipePDS <felipepdasilva66@gmail.com>
2021-03-21 16:33:25 -0300

    Remove bugs

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -70,7 +70,6 @@ themes.buttons.containerSetTheme.addEventListener('click', () => {
     game.entities.box.events.renderBox()
 })
 
-localStorage.setItem('Theme', themes.buttons.buttonSetTheme.checked ? 'dark' : 'light')
+if (localStorage.Theme === undefined) localStorage.setItem('Theme', 'dark')
 setTheme(localStorage.Theme)
 localStorage.Theme === 'dark' ? themes.buttons.buttonSetTheme.checked = true : themes.buttons.buttonSetTheme.checked = false
-console.log(localStorage.Theme)
